9bb3aa7120c05d95810a7b22ebcb02351c761582
Stephen T Cannon <stephentcannon@gmail.com>
2014-10-24 16:49:29 -0400

    up 2 meteor 0.9.0

--- package.js
+++ package.js
@@ -1,26 +1,33 @@
 Package.describe({
-  summary: "A meteorite package for a Bootstrap 3 Googlesque login, sign up and password reminder form and functionality."
+  summary: "A meteor package for a Bootstrap 3 Googlesque login, sign up and password reminder",
+  // Version number.
+  version: "0.2.4",
+  // Optional.  Default is package directory name.
+  name: "steeve:reactive-cookie",
+  // Optional github URL to your source repository.
+  git: "https://github.com/EtherPOS/single-page-login.git",
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.0');
   api.use([
     'templating',
     'accounts-base',
-    'accounts-password', 
+    'accounts-password',
     'accounts-ui',
     'handlebars',
     ], 'client');
 
-  api.add_files([
-    'client/single_page_login.css', 
-    'client/single_page_login.html', 
+  api.addFiles([
+    'client/single_page_login.css',
+    'client/single_page_login.html',
     'client/single_page_login.js',
     'client/helpers.js',
-    ], 
+    ],
   'client');
 
-  api.add_files('shared/SinglePageLogin.js', ['client', 'server']);
+  api.addFiles('shared/SinglePageLogin.js', ['client', 'server']);
 
-  api.use('iron-router', ['client']);
-  api.use('bootstrap-3', ['client']);
+  api.use('iron:router@0.9.4', 'client');
+  api.use('mizzao:bootstrap-3@3.2.0_1', 'client');
 });
